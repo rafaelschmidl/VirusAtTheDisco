b06bfdca15df2deb237229c5defbfb246891e725
rafaelschmidl <schmidlrafael@gmail.com>
2020-09-17 19:39:27 +0200

    ...

--- VirusAtTheDisco/Person.cs
+++ VirusAtTheDisco/Person.cs
@@ -7,16 +7,16 @@ namespace VirusAtTheDisco
     class Person
     {
         public bool IsInfected { get; set; }
+        public bool IsImune { get; set; }
         public int TimePersonIsContagious { get; set; }
         public int TimeSinceInfection { get; set; }
-        public bool IsImune { get; set; }
 
         public Person()
         {
             IsInfected = false;
+            IsImune = false;
             TimePersonIsContagious = 5;
             TimeSinceInfection = 0;
-            IsImune = false;
         }
     }
 }
